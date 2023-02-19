412aff5d49c1c554024acf91c4b68b81145e3819
Eshita Islam <islam.eshita@gmail.com>
2023-02-18 17:55:27 -0800

    Added code for drawing a basic board

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello Snakes and Ladders')
+function drawBoard()
+{
+    const gameBoard = document.getElementById('gameBoard');
+
+    for(let i=0; i<100; i++)
+    {
+        const numberElem = document.createElement('div');
+        let value = String(100 - i);
+        numberElem.textContent = value;
+        numberElem.setAttribute('class', 'boardGrid');
+        numberElem.setAttribute('id', `boardGrid${value}`);
+        gameBoard.append(numberElem);
+    }
+}
+
+document.body.onload = function()
+{
+    drawBoard();
+}
